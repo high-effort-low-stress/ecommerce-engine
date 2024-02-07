9e18177ca53e5b0234539b2ef4b5ea852d7c3349
iago <iago.scandido3@gmail.com>
2024-02-06 21:07:12 -0300

    initial commit

---
+++ src/test/java/com/hels/ecommerceengine/EcommerceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hels.ecommerceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
