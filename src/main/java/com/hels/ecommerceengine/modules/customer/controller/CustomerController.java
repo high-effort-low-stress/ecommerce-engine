7e36ef9fbdcd57d86aa4aa6ce25ea973107a9668
iago <iago.scandido3@gmail.com>
2024-02-24 15:25:10 -0300

    TODO: fix customer controller bean

---
+++ src/main/java/com/hels/ecommerceengine/modules/customer/controller/CustomerController.java
@@ -0,0 +1,33 @@
+package com.hels.ecommerceengine.modules.customer.controller;
+
+import com.hels.ecommerceengine.modules.customer.dto.CreateCustomerAccountDTO;
+import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
+import com.hels.ecommerceengine.modules.customer.mapper.CustomerMapper;
+import com.hels.ecommerceengine.modules.customer.repository.ICustomerRepository;
+import com.hels.ecommerceengine.modules.customer.service.CreateCustomerAccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    private final ICustomerRepository repository;
+    private final CreateCustomerAccountService createCustomerAccountService;
+    private final CustomerMapper mapper;
+
+//    @PostMapping
+//    public CreateCustomerAccountDTO.Response customerCreateAccount (@RequestBody CreateCustomerAccountDTO.Request requestBody) {
+//        CustomerEntity customer = createCustomerAccountService.execute(mapper.toCustomerDTO(requestBody));
+//
+//        return new CreateCustomerAccountDTO.Response(customer.getId().toString());
+//    }
+
+    @GetMapping("/all")
+    public List<CustomerEntity> findAll () {
+        return repository.findAll();
+    }
+
+}
