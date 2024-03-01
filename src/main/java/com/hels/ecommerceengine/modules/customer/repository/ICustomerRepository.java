2b78e29781f17cdb3c9e473538c8afbe538d5870
iago silva <iago.scandido3@gmail.com>
2024-03-01 15:31:50 -0300

    fix: add param to query

--- src/main/java/com/hels/ecommerceengine/modules/customer/repository/ICustomerRepository.java
+++ src/main/java/com/hels/ecommerceengine/modules/customer/repository/ICustomerRepository.java
@@ -3,6 +3,7 @@ package com.hels.ecommerceengine.modules.customer.repository;
 import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,15 +11,16 @@ import java.util.Optional;
 @Repository
 public interface ICustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
-    @Query(value = "SELECT * FROM ecommerce.customer c " +
+    @Query(
+            value = "SELECT * FROM ecommerce.customer " +
             "WHERE document=:document AND email=:email AND phone_number=:phoneNumber AND is_active=true",
-            nativeQuery = true)
-    Optional<CustomerEntity> findByDocumentAndEmailAndPhoneNumber(String document, String email, String phoneNumber);
+            nativeQuery = true
+    )
+    Optional<CustomerEntity> findByDocumentAndEmailAndPhoneNumber(
+            @Param("document") String document,
+            @Param("email") String email,
+            @Param("phoneNumber") String phoneNumber
+    );
 
-    @Override
-    @Query(value = "SELECT * FROM ecommerce.customer " +
-            "WHERE id=:id AND is_active=true",
-    nativeQuery = true)
-    Optional<CustomerEntity> findById(Long id);
 }
 
