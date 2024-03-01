9ce077c78852e9e2b9281eca7a21749a7984d85d
iago silva <iago.scandido3@gmail.com>
2024-02-29 22:17:23 -0300

    feat: add get customer

--- src/main/java/com/hels/ecommerceengine/modules/customer/controller/CustomerController.java
+++ src/main/java/com/hels/ecommerceengine/modules/customer/controller/CustomerController.java
@@ -1,14 +1,13 @@
 package com.hels.ecommerceengine.modules.customer.controller;
 
 import com.hels.ecommerceengine.modules.customer.dto.CreateCustomerDto;
+import com.hels.ecommerceengine.modules.customer.dto.GetCustomerDto;
 import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
 import com.hels.ecommerceengine.modules.customer.service.CreateCustomerService;
+import com.hels.ecommerceengine.modules.customer.service.GetCustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,11 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final CreateCustomerService createCustomerService;
+    private final GetCustomerService getCustomerService;
 
     @PostMapping
-    public CreateCustomerDto.Response customerCreateAccount (@Valid @RequestBody CreateCustomerDto.Request requestBody) {
+    public CreateCustomerDto.Response createAccount(@Valid @RequestBody CreateCustomerDto.Request requestBody) {
         CustomerEntity customer = createCustomerService.execute(requestBody);
 
         return new CreateCustomerDto.Response(customer.getId().toString());
+    }
+
+    @GetMapping("/{id}")
+    public GetCustomerDto.Response getCustomer (@PathVariable Long id) {
+        CustomerEntity customer = getCustomerService.execute(id);
+
+        return new GetCustomerDto.Response(customer.getName(), customer.getDocument(),
+                customer.getEmail(), customer.getPhoneNumber(), customer.getBirthDate());
     }
 }
