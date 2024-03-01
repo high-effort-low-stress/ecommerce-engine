ad0b23d516c1457fb91e40d176a538dac3734996
iago silva <iago.scandido3@gmail.com>
2024-02-29 22:17:56 -0300

    feat: add mappers to get customer, refact: remove default

--- src/main/java/com/hels/ecommerceengine/modules/customer/mapper/CustomerMapper.java
+++ src/main/java/com/hels/ecommerceengine/modules/customer/mapper/CustomerMapper.java
@@ -1,23 +1,16 @@
 package com.hels.ecommerceengine.modules.customer.mapper;
 
 import com.hels.ecommerceengine.modules.customer.dto.CreateCustomerDto;
+import com.hels.ecommerceengine.modules.customer.dto.GetCustomerDto;
 import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-    default CreateCustomerDto.Request toEntity (CustomerEntity entity) {
-        if (entity == null)
-            return null;
-        CreateCustomerDto.Request dto = new CreateCustomerDto.Request();
-        dto.setName(entity.getName());
-        dto.setDocument(entity.getDocument());
-        dto.setEmail(entity.getEmail());
-        dto.setPassword(entity.getPassword());
-        dto.setBirthDate(entity.getBirthDate());
-        dto.setPhoneNumber(entity.getPhoneNumber());
-        return dto;
-    }
     CustomerEntity toEntity(CreateCustomerDto.Request request);
+
+    GetCustomerDto.Response entityToGetCustomerDtoResponse (CustomerEntity entity);
+
+    CustomerEntity getCustomerDtoResponseToEntity (GetCustomerDto.Response response);
 }
