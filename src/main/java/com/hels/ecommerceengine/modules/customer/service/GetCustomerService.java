5a6ae78aa9d3a1cad8b1f2ea6affa089d5a6d881
iago silva <iago.scandido3@gmail.com>
2024-03-01 15:30:44 -0300

    perf: remove map and return entity

--- src/main/java/com/hels/ecommerceengine/modules/customer/service/GetCustomerService.java
+++ src/main/java/com/hels/ecommerceengine/modules/customer/service/GetCustomerService.java
@@ -1,9 +1,7 @@
 package com.hels.ecommerceengine.modules.customer.service;
 
 import com.hels.ecommerceengine.exceptions.ApiException;
-import com.hels.ecommerceengine.modules.customer.dto.GetCustomerDto;
 import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
-import com.hels.ecommerceengine.modules.customer.mapper.CustomerMapper;
 import com.hels.ecommerceengine.modules.customer.repository.ICustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,16 +12,13 @@ import java.util.Objects;
 @Service
 public class GetCustomerService {
     private final ICustomerRepository repository;
-    private final CustomerMapper mapper;
     public CustomerEntity execute (Long id) {
         CustomerEntity customer = repository.findById(id).orElse(null);
 
         if (Objects.isNull(customer))
             throw new ApiException("Customer not found");
 
-        GetCustomerDto.Response response = mapper.entityToGetCustomerDtoResponse(customer);
-
-        return mapper.getCustomerDtoResponseToEntity(response);
+        return customer;
     }
 
 }
