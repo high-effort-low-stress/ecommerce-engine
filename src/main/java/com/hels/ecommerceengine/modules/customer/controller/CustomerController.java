91a59c178e102c1e9280b6e8633bb123728d050e
iago silva <iago.scandido3@gmail.com>
2024-03-02 00:31:38 -0300

    feat: add soft delete method

--- src/main/java/com/hels/ecommerceengine/modules/customer/controller/CustomerController.java
+++ src/main/java/com/hels/ecommerceengine/modules/customer/controller/CustomerController.java
@@ -2,12 +2,15 @@ package com.hels.ecommerceengine.modules.customer.controller;
 
 import com.hels.ecommerceengine.modules.customer.dto.CreateCustomerDto;
 import com.hels.ecommerceengine.modules.customer.dto.GetCustomerDto;
+import com.hels.ecommerceengine.modules.customer.dto.InactiveCustomerDto;
 import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
 import com.hels.ecommerceengine.modules.customer.mapper.CustomerMapper;
 import com.hels.ecommerceengine.modules.customer.service.CreateCustomerService;
 import com.hels.ecommerceengine.modules.customer.service.GetCustomerService;
+import com.hels.ecommerceengine.modules.customer.service.InactiveCustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -18,8 +21,9 @@ public class CustomerController {
     private final CreateCustomerService createCustomerService;
     private final GetCustomerService getCustomerService;
     private final CustomerMapper mapper;
-
+    private final InactiveCustomerService inactiveCustomerService;
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public CreateCustomerDto.Response createAccount(@Valid @RequestBody CreateCustomerDto.Request requestBody) {
         CustomerEntity customer = createCustomerService.execute(requestBody);
 
@@ -27,9 +31,17 @@ public class CustomerController {
     }
 
     @GetMapping("/{id}")
-    public GetCustomerDto.Response getCustomer (@PathVariable Long id) {
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public GetCustomerDto.Response getCustomer (@PathVariable("id") Long id) {
         CustomerEntity customer = getCustomerService.execute(id);
 
         return mapper.entityToGetCustomerDtoResponse(customer);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void inactiveCustomer (@PathVariable("id") Long id) {
+        CustomerEntity customer = inactiveCustomerService.execute(id);
+    }
+
 }
