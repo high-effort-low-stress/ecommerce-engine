b31edfaf84d5a7c86bbd0a64bc6e03d9ff2a1213
iago silva <iago.scandido3@gmail.com>
2024-03-02 00:34:05 -0300

    feat: add inactive by id service

---
+++ src/main/java/com/hels/ecommerceengine/modules/customer/service/InactiveCustomerService.java
@@ -0,0 +1,28 @@
+package com.hels.ecommerceengine.modules.customer.service;
+
+import com.hels.ecommerceengine.exceptions.ApiException;
+import com.hels.ecommerceengine.modules.customer.entity.CustomerEntity;
+import com.hels.ecommerceengine.modules.customer.repository.ICustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class InactiveCustomerService {
+    private final ICustomerRepository repository;
+
+    public CustomerEntity execute(Long id) {
+        CustomerEntity customer = repository.findById(id).orElse(null);
+
+        if (Objects.isNull(customer))
+            throw new ApiException("Customer not found");
+
+        customer.setActive(false);
+
+        return repository.save(customer);
+    }
+
+
+}
